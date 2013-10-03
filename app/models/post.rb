f556c500afdd8e68e59ed431c69018ee76503aab
Sateesh N <sateesh.naramsetti@gmail.com>
2013-10-03 14:02:38 +0530

    Created a rails application and added status updates

---
+++ app/models/post.rb
@@ -0,0 +1,3 @@
+class Post < ActiveRecord::Base
+  attr_accessible :address, :desire, :dob, :hobby, :interest, :name, :signature
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slambook::Application.config.secret_token = '1f97a002a431e5e82a919241dad0ea9e0d01c3cd8089a923c4f7e97f68289b5d5fafde7d931299e6516323f33a7c9d8ff00ded3be899ea76183fd5407ce8085a'
